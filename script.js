fdf2f7ed8e4d346869210c1feec3ac6e4a5404cc
ayusharma-ctrl <117670536+ayusharma-ctrl@users.noreply.github.com>
2022-12-13 15:19:38 +0530

    done

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+let evaluatedText = document.getElementById('evaluatedText')
+let letterCount = document.getElementById('letterCount')
+
+evaluatedText.onkeyup = function(){
+    letterCount.innerHTML = evaluatedText.value.length
+}
+
+
+
